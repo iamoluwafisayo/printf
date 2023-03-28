febdd91bc3c2bf3e5b89fc7587164bd4fce31ac6
Eno-obong Ukpong <enoobongvin@gmail.com>
2023-03-28 12:11:16 -0700

    Add _printf.c

---
+++ test/_printf_test.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: test for _printf function
+ * Return: Always 0
+ */
+int main(void)
+{
+	_printf("Let's try to printf a simple sentence.\n");
+
+	return (0);
+}
